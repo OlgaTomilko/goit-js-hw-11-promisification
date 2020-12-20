518a31dd6db6218a31ed365370c94de68d31d15c
OlgaTomilko <67008325+OlgaTomilko@users.noreply.github.com>
2020-12-20 21:43:50 +0300

    Правки

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@
 
 // To install it, you can run: npm install --save core-js/fn/promise
 import './styles.css';
-// import './task1';
-// import './task2';
+import './task1';
+import './task2';
 import './task3';
